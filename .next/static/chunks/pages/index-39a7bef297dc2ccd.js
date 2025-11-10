eca55be79c08a3899788a7eef999a388864f3c10
Alwin K G <alwinkgofficial@gmail.com>
2025-11-10 09:59:10 +0530

    update on resume

--- .next/static/chunks/pages/index-39a7bef297dc2ccd.js
+++ .next/static/chunks/pages/index-39a7bef297dc2ccd.js
@@ -324,7 +324,7 @@
                           ],
                         }),
                         (0, a.jsx)(d.E.a, {
-                          href: "https://drive.google.com/file/d/1HozMJ3XVIZsfem091npmFZQqVybi9hQ1/view?usp=sharing",
+                          href: "https://drive.google.com/file/d/1HozMJ3XVIZsfem091npmFZQqVybi9hQ1/view",
                           target: "_blank",
                           rel: "noopener noreferrer",
                           className:
